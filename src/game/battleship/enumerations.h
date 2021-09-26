e9ee363a069c6dd915c6c31cd3a7113872213b0c
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-26 22:47:02 +0300

    feat: bitwise operations for enum(TileState)

--- src/game/battleship/enumerations.h
+++ src/game/battleship/enumerations.h
@@ -1,11 +1,54 @@
-#pragma once
+
+#ifndef __ENUMERATIONS_H_INCLUDED
+#define __ENUMERATIONS_H_INCLUDED
+
 namespace Battleship {
-enum class TileState { Empty = 0, WasShot = 1, ShipAfloat = 2, ShipSunk = 4 };
-enum class TurnStatus { Ok = 0, TileError = 1, OrderError = 2 };
-enum class MappingStatus { Ok = 0, PlacementError = 1, WrongPhaseError = 2 };
-enum class TerminationStatus {
-  NotTerminated = 0,
-  PlayerWon = 1,
-  PlayerLost = 2
-};
+    enum class TileState {
+        Empty = 0,
+        WasShot = 1,
+        ShipAfloat = 2,
+        ShipSunk = 4
+    };
+
+    enum class TurnStatus {
+        Ok = 0,
+        TileError = 1,
+        OrderError = 2
+    };
+
+    enum class MappingStatus {
+        Ok = 0,
+        PlacementError = 1,
+        WrongPhaseError = 2
+    };
+
+    enum class TerminationStatus {
+        NotTerminated = 0,
+        PlayerWon = 1,
+        PlayerLost = 2
+    };
+
+
+
+    inline TileState operator|(TileState lhs, TileState rhs) {
+        return static_cast<TileState>(static_cast<int>(lhs) | static_cast<int>(rhs));
+    }
+
+    inline TileState& operator|=(TileState& lhs, const TileState& rhs) {
+        return lhs = lhs | rhs;
+    }
+
+    inline TileState operator&(TileState lhs, TileState rhs) {
+        return static_cast<TileState>(static_cast<int>(lhs) & static_cast<int>(rhs));
+    }
+
+    inline TileState& operator&=(TileState& lhs, const TileState& rhs) {
+        return lhs = lhs & rhs;
+    }
+
+    inline bool operator==(TileState lhs, TileState rhs) {
+        return static_cast<int>(lhs) == static_cast<int>(rhs);
+    }
 } // namespace Battleship
+
+#endif // __ENUMERATIONS_H_INCLUDED
