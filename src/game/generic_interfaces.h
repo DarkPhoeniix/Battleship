c8ec2a2c7cd5686ae4f120deab58580b4f60226e
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-29 06:53:32 +0300

    fix: added library for int

--- src/game/generic_interfaces.h
+++ src/game/generic_interfaces.h
@@ -1,18 +1,32 @@
-#pragma once
+
+#ifndef __GENERIC_INTERFACES_H_INCLUDED
+#define __GENERIC_INTERFACES_H_INCLUDED
+
+#include <cstdint>
 #include <utility>
+
 namespace Interfaces {
-template <typename TileState> struct IMapScanner {
-  virtual TileState getTileState(uint8_t x, uint8_t y,
-                                 uint8_t playerId = 0) = 0;
-};
-template <typename TurnStatus> struct ITurnRegistrar {
-  virtual TurnStatus registerTurn(uint8_t x, uint8_t y,
-                                  uint8_t playerId = 0) = 0;
-};
-template <typename MappingStatus> struct IMapper {
-  virtual MappingStatus addTile(uint8_t x, uint8_t y, uint8_t playerId = 0) = 0;
-};
-template <typename TerminationStatus> struct ITerminationChecker {
-  virtual TerminationStatus finished(uint8_t playerId = 0) = 0;
-};
+    template <typename TileState> struct IMapScanner {
+        virtual TileState getTileState(uint8_t xCoordinate,
+                                       uint8_t yCoordinate,
+                                       uint8_t playerId = 0) = 0;
+    };
+
+    template <typename TurnStatus> struct ITurnRegistrator {
+        virtual TurnStatus registerTurn(uint8_t xCoordinate,
+                                        uint8_t yCoordinate,
+                                        uint8_t playerId = 0) = 0;
+    };
+
+    template <typename MappingStatus> struct IMapper {
+        virtual MappingStatus addTile(uint8_t xCoordinate,
+                                      uint8_t yCoordinate,
+                                      uint8_t playerId = 0) = 0;
+    };
+
+    template <typename TerminationStatus> struct ITerminationChecker {
+        virtual TerminationStatus finished(uint8_t playerId = 0) = 0;
+    };
 } // namespace Interfaces
+
+#endif // __GENERIC_INTERFACES_H_INCLUDED
